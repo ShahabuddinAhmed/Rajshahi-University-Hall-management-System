369431ad1c26762e04a3d5531e7e511538927037
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2017-07-04 23:58:11 +0600

    Infos Table is created

---
+++ database/migrations/2017_07_03_182114_create_infos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInfosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('infos', function (Blueprint $table) {
+            $table->string('about', 1000);
+            $table->string('administration', 1000);
+            $table->string('library', 1000);
+            $table->string('dining', 1000);
+            $table->string('canteen', 1000);
+            $table->string('contact', 1000);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('infos');
+    }
+}
