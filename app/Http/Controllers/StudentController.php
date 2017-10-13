94cbcca58695c0f7f902f1db936430184a8389c1
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2017-10-13 21:39:24 +0600

    Student controller is modified

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\StudentComplain;
 
 class StudentController extends Controller
 {
@@ -19,5 +20,16 @@ class StudentController extends Controller
     public function viewMyPayment()
     {
     	return view('student.viewMyPayment');
+    }
+
+    public function postStudent(Request $request)
+    {
+        $complain = new StudentComplain;
+        $complain->name = $request['name'];
+        $complain->std_id = $request['std_id'];
+        $complain->room = $request['room'];
+        $complain->complain = $request['complain'];
+        $complain->save();
+        return view('/student.student');
     }
 }
